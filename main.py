a92e605bfa74a9a38b3b9d69ab72f597823150f5
marcotommasini <marco.t.coelho@hotmail.com>
2022-10-20 10:55:40 -0300

    Main

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 from utils import get_statistical_parameters
 from functions import operations as op
 
-def main(params):
+def main():
     parser = argparse.ArgumentParser(description='Diffusion model')
 
     parser.add_argument('--device', type=str, default="cuda", help='Device to run the code on')
@@ -27,12 +27,11 @@ def main(params):
     parser.add_argument('--number_epochs', type=int, default=50, help='Number of epochs the model will learn from')
     parser.add_argument('--initial_learning_rate', type=float, default=1e-6, help='Initial learning rate of the optmizer')
     parser.add_argument('--final_learning_rate', type=float, default=1e-4, help='Initial learning rate of the optmizer')
-    parser.add_argument('--checkpoint_directory', type=str, default="", help='')
-    parser.add_argument('--dataset_directory', type=str, default="", help='')
-    parser.add_argument('--zipped_dataset_directory', type=str, default="", help='')
-    parser.add_argument('--get_mean_std', type=int, default=0, help='Boolean for gathering the dataset MEAN and STD for normalization')
+    parser.add_argument('CD','--checkpoint_directory', type=str, default="", help='Input Checkpoints directory')
+    parser.add_argument('DD', '--dataset_directory', type=str, default="", help='FIle with images')
+    parser.add_argument('ZDD', '--zipped_dataset_directory', type=str, default="", help='Zipped file to be unzipped')
 
-    args = parser.parse_args(params)
+    args = parser.parse_args()
 
     #UNZIP the file and get statistical parameters
     stats_HIGH, stats_LOW = get_statistical_parameters(args.zipped_dataset_directory, args.dataset_directory,batch_size=args.batch_size)
@@ -52,7 +51,7 @@ def main(params):
     op_object.train_model(model, dataloader, optmizer, loss)
 
 if __name__ == "__main__":
-    main([])
+    main()
 
 
 
